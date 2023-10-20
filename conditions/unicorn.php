8f8f2251c65753c492b17cc0076a2d1f99ca7bc8
Virginie Dourson <virginiedourson@gmail.com>
2023-10-20 09:42:16 +0200

    Finish unicorn

--- conditions/unicorn.php
+++ conditions/unicorn.php
@@ -11,8 +11,14 @@
     <input type="submit" name="submit" value="Submit">
 </form>
 <?php  
- if (isset($_GET['what'])){
-
- }
+if (isset($_GET['what'])){
+    $what = $_GET['what'];
+    $imgCat = '<img src="cat.gif" alt="gif of a cat">';
+    $imgHuman = '<img src="https://media.giphy.com/media/l3nWflasE5VWXHdRu/giphy.gif" alt="gif of a human">';
+    $imgLicorn = '<img src="https://media.giphy.com/media/j0kQJxo5mzGYb4EvWK/giphy.gif" alt="gif of a licorn">';
+    $image = ($what == 'cat') ? $imgCat : (($what == 'human') ? $imgHuman : $imgLicorn);
+    echo $image;
+}
 ?>
-<img src="https://media.giphy.com/media/l3nWflasE5VWXHdRu/giphy.gif"></img>
+
+
